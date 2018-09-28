a99de594659ef86fec8e8a8cb8f0b65d59d93f2e
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-09-28 02:48:13 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,25 @@ require 'pry'
 
 class String
 
+
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
+    if sentence? == true || question? == true || exclamation? == true
+      self_array = self.split(/(([.?!])\a*)/)
+      sentences = self_array.count
+    end
+binding.pry
 
   end
 end
